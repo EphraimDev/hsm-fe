42c0bbd17aa3046025d62f34983a3fed34b930cf
olucheye <seyefree2rhyme@yahoo.com>
2019-11-19 18:14:26 +0100

    second commit

--- Pages/index.js
+++ Pages/index.js
@@ -3,8 +3,8 @@ import Layout from '../components/MyLayout';
 export default function index(){
 
     return (
+        
         <Layout>
-
             <div className="container mt-5">
 
                 <div className="text-center mb-5">
@@ -13,18 +13,18 @@ export default function index(){
                 </div>
             
                 <form className="bg-light p-3 col-md-6 col-lg-6 mx-auto">
-                    <div class="form-group ">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <div className="form-group ">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
         </Layout>
--- components/Header.js
+++ components/Header.js
@@ -12,11 +12,11 @@ const Header = () => (
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
 
-                <li class="nav-item active">
+                <li className="nav-item active">
                 <Link href="/"><a className="nav-link">Home</a></Link>
                 </li>
 
-                <li class="nav-item">
+                <li className="nav-item">
                 <Link href="/about"><a className="nav-link">About</a></Link>
                 </li>
 
